a69209d8f2f473aa1e01e4d2153f1ff7c9a8b436
fuzzguard <fuzzguard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-30 07:31:38 +0000

    Gold Release of Privilege Menu
    git-svn-id: https://plugins.svn.wordpress.org/privilege-menu/trunk@884517 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ privMenu.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Plugin Name: Privileged Menu
+ * Plugin URI: http://www.fuzzguard.com.au/plugins/privileged-menu
+ * Description: Used to provide Menu display to users based on their Privilege Level (Currently only either logged in/logged out)
+ * Version: 1.0
+ * Author: Benjamin Guy
+ * Author URI: http://www.fuzzguard.com.au
+ * License: GPL2
+
+    Copyright 2014  Benjamin Guy  (email: beng@fuzzguard.com.au)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+
+/**
+* Don't display if wordpress admin class is not found
+* Protects code if wordpress breaks
+* @since 0.2
+*/
+if ( ! function_exists( 'is_admin' ) ) {
+    header( 'Status: 403 Forbidden' );
+    header( 'HTTP/1.1 403 Forbidden' );
+    exit();
+}
+
+/**
+* Load Custom Walker class
+* @since 0.2
+*/
+include('customWalker.php');
+
+
+
+/**
+* Create class privMenu() to prevent any function name conflicts with other WordPress plugins or the WordPress core.
+* @since 0.1
+*/
+class privMenu {
+
+
+
+
+    	/**
+     	* Removes items from the menu displayed to the user if that menu item has been denied access to them in the admin panel
+     	* @since 0.2
+     	*/
+	function remove_menu_items( $items, $menu, $args ) {
+
+    		foreach ( $items as $key => $item ) {
+			$meta_data = get_post_meta( $item->ID, '_priv_menu_role', true);
+          		switch( $meta_data ) {
+            			case 'in' :
+              				$visible = is_user_logged_in() ? true : false;
+              				break;
+            			case 'out' :
+              				$visible = ! is_user_logged_in() ? true : false;
+              				break;
+            			default:
+	      				$visible = true;
+				/*
+              				$visible = false;
+              				if ( is_array( $item->roles ) && ! empty( $item->roles ) ) foreach ( $item->roles as $role ) {
+                				if ( current_user_can( $role ) ) $visible = true;
+              				}
+				*/
+              				break;
+          		}
+          		// add filter to work with plugins that don't use traditional roles
+          		$visible = apply_filters( 'nav_menu_roles_item_visibility', $visible, $item );
+
+          		if ( ! $visible ) unset( $items[$key] ) ;
+    		}
+
+    		return $items;
+	}
+
+    /**
+     * Replace the default Admin Menu Walker
+     * @since 0.2
+     */
+    function edit_priv_menu_walker( $walker, $menu_id ) {
+        return 'Priv_Menu_Walker';
+    }
+
+
+
+    /**
+     * Save users selection in DataBase as post_meta on return of data from users browser
+     * @since 0.2
+     */
+    function save_extra_menu_opts( $menu_id, $menu_item_db_id, $args ) {
+        global $wp_roles;
+
+        $allowed_roles = apply_filters( 'priv_menu_roles', $wp_roles->role_names );
+
+        // verify this came from our screen and with proper authorization.
+        if ( ! isset( $_POST['priv-menu-role-nonce'] ) || ! wp_verify_nonce( $_POST['priv-menu-role-nonce'], 'priv-menu-nonce-name' ) )
+            return;
+
+        $saved_data = false;
+
+        if ( isset( $_POST['priv-menu-logged-in-out'][$menu_item_db_id]  )  && in_array( $_POST['priv-menu-logged-in-out'][$menu_item_db_id], array( 'in', 'out' ) ) ) {
+              $saved_data = $_POST['priv-menu-logged-in-out'][$menu_item_db_id];
+        } elseif ( isset( $_POST['priv-menu-role'][$menu_item_db_id] ) ) {
+            $custom_roles = array();
+            // only save allowed roles
+            foreach( $_POST['priv-menu-role'][$menu_item_db_id] as $role ) {
+                if ( array_key_exists ( $role, $allowed_roles ) ) $custom_roles[] = $role;
+            }
+            if ( ! empty ( $custom_roles ) ) $saved_data = $custom_roles;
+        }
+
+        if ( $saved_data ) {
+            update_post_meta( $menu_item_db_id, '_priv_menu_role', $saved_data );
+        } else {
+            delete_post_meta( $menu_item_db_id, '_priv_menu_role' );
+        }
+    }
+
+
+
+} //End of privMenu() class
+
+
+
+/**
+* Define the Class
+* @since 0.1
+*/
+$myprivMenuClass = new privMenu();
+
+
+/**
+* Action of what function to call to save users selection when returned from their browser
+* @since 0.1
+*/
+add_action( 'wp_update_nav_menu_item', array( $myprivMenuClass, 'save_extra_menu_opts'), 10, 3 );
+
+
+/**
+* Replace the default Admin Menu Walker with the custom one from the customWalker.php file
+* @since 0.1
+*/
+add_filter( 'wp_edit_nav_menu_walker', array( $myprivMenuClass, 'edit_priv_menu_walker' ), 10, 2 );
+
+
+/**
+* If is_admin() is not defined (User not in admin panel) then filter the displayed menu through the below function.
+* @since 0.2
+*/
+if ( ! is_admin() ) {
+        // add meta to menu item
+	add_filter( 'wp_get_nav_menu_items', array($myprivMenuClass, 'remove_menu_items'), null, 3 );
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Privilege Menu ===
+Contributors: fuzzguard
+Donate link:
+tags: nav menu, nav menus, menus, menu
+Requires at least: 3.8
+Tested up to: 3.8
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+This plugin allows you to display menu items based on if a user is logged in, logged out or both.
+
+This solves the problem of having to modify theme functions.php files to add in menu restrictions based on users logged in/logged out status.  The changes in functions.php is often overwritten by a theme update.  This plugin removes that worry, as you can update the theme, independant of the plugin.
+
+== Installation ==
+
+1. Upload the `plugin` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Go to Appearance > Menus
+1. Edit the menu items accordingly.  First select whether you'd like to display the item to all logged in users, all logged out users or Both (Default).
+1. Save the changes to the menu.  You should now see excluded menu items based on a users logged in/logged out state.
+
+== Frequently Asked Questions ==
+
+= I cannot see the options for Privilege Menu under menu items in the Admin Panel? =
+
+This usually occurs due to a plugin conflict.  The Admin Menu Walker can only have one custom walker so any other plugin that uses a custom Admin Menu Walker will cause conflicts with Privilege Menu plugin.
+
+Wordpress does not yet have sufficient hooks in this area of the admin panel.  Due to this plugins are forced to load a modifed custom Admin Menu Walker.  The custom Admin Menu Walker is limited to one so only one Admin Panel menu modification plugin can be active at one time.
+
+Although this feature has been requested since 3.6 it still hasn't been added to the WordPress Core.
+There's a possibility that support for "Nav Menu UI" Hooks will be added in WordPress 4.0:
+http://core.trac.wordpress.org/ticket/18584
+
+== Screenshots ==
+
+1. The is a view of the Admin Panel Nav Menu editing area.  The extra selectable options are added to each menu item.
+2. This is the menu a logged IN user would see.  These users don't see "Login" due to the fact that it is denied to logged IN users in the Admin Panel Nav Menus section.
+3. This is the menu a logged OUT user would see.  These users don't see "Logout", "My Account" or "Control Panel" due to the fact that they are denied to logged OUT users in the Admin Panel Nav Menus section.
+
+== Changelog ==
+
+= 1.0 =
+* Gold release
